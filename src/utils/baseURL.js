b19fb21ccd50e1310ab8b053fd0dbc4db602a8c9
Ujang Arisandi <ujangaja@gmail.com>
2023-11-17 20:28:17 +0700

    update path link

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,4 +1,4 @@
-const BASE_URL = `https://sponge-apparent-satyr.ngrok-free.app/perpus-api/v1`;
-// const BASE_URL = `http://localhost:5000/perpus-api/v1`;//ini nyalain kalau mau coba di local
+// const BASE_URL = `https://sponge-apparent-satyr.ngrok-free.app/perpus-api/v1`;
+const BASE_URL = `http://localhost:5000/perpus-api/v1`; //ini nyalain kalau mau coba di local
 
 export default BASE_URL;
